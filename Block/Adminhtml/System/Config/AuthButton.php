be6beca1ee1471220dcd71a6b4cd4f54a3d12fae
GARDENLAWN <rpcsky@gmail.com>
2026-01-16 19:08:55 +0100

    Init

--- Block/Adminhtml/System/Config/AuthButton.php
+++ Block/Adminhtml/System/Config/AuthButton.php
@@ -4,13 +4,22 @@ namespace GardenLawn\TransEu\Block\Adminhtml\System\Config;
 use Magento\Config\Block\System\Config\Form\Field;
 use Magento\Framework\Data\Form\Element\AbstractElement;
 use GardenLawn\TransEu\Model\AuthService;
+use Magento\Backend\Block\Template\Context;
 
 class AuthButton extends Field
 {
+    /**
+     * @var AuthService
+     */
     protected $authService;
 
+    /**
+     * @param Context $context
+     * @param AuthService $authService
+     * @param array $data
+     */
     public function __construct(
-        \Magento\Backend\Block\Template\Context $context,
+        Context $context,
         AuthService $authService,
         array $data = []
     ) {
@@ -18,13 +27,32 @@ class AuthButton extends Field
         $this->authService = $authService;
     }
 
+    /**
+     * @param AbstractElement $element
+     * @return string
+     */
     protected function _getElementHtml(AbstractElement $element)
     {
-        $url = $this->authService->getAuthorizationUrl();
-        $label = __('Authorize with Trans.eu');
+        try {
+            $url = $this->authService->getAuthorizationUrl();
+        } catch (\Exception $e) {
+            return '<div class="message message-error error"><div>' . __('Error generating auth URL: %1', $e->getMessage()) . '</div></div>';
+        }
 
-        $html = '<a href="' . $url . '" class="action-default" target="_blank">' . $label . '</a>';
-        $html .= '<p class="note"><span>' . __('Click to authorize the application with Trans.eu. You will be redirected back after login.') . '</span></p>';
+        /** @var \Magento\Backend\Block\Widget\Button $button */
+        $button = $this->getLayout()->createBlock(
+            \Magento\Backend\Block\Widget\Button::class
+        );
+
+        $button->setData([
+            'id' => 'trans_eu_auth_btn',
+            'label' => __('Authorize with Trans.eu'),
+            'onclick' => 'window.open(\'' . $url . '\', \'_blank\')',
+            'class' => 'action-default primary'
+        ]);
+
+        $html = $button->toHtml();
+        $html .= '<p class="note"><span>' . __('Clicking this button will open the Trans.eu authorization page in a new tab.') . '</span></p>';
 
         return $html;
     }
