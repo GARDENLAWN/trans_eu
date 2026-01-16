5e41c506734221a24d3e9e6f53fe2a482c2d92ea
GARDENLAWN <rpcsky@gmail.com>
2026-01-16 19:10:04 +0100

    Init

--- Controller/Index/Index.php
+++ Controller/Index/Index.php
@@ -23,21 +23,33 @@ class Index extends Action
         $code = $this->getRequest()->getParam('code');
         $error = $this->getRequest()->getParam('error');
 
+        /** @var \Magento\Framework\Controller\Result\Raw $result */
+        $result = $this->resultFactory->create(ResultFactory::TYPE_RAW);
+        $result->setHeader('Content-Type', 'text/html');
+
+        $html = '<!DOCTYPE html><html><head><title>Trans.eu Authorization</title><style>body{font-family:sans-serif;text-align:center;padding:50px;}</style></head><body>';
+
         if ($error) {
-            $this->messageManager->addErrorMessage(__('Trans.eu authorization failed: %1', $error));
+            $html .= '<h1 style="color:red;">Authorization Failed</h1>';
+            $html .= '<p>' . htmlspecialchars($error) . '</p>';
         } elseif ($code) {
             try {
                 $this->authService->handleCallback($code);
-                $this->messageManager->addSuccessMessage(__('Trans.eu authorization successful.'));
+                $html .= '<h1 style="color:green;">Authorization Successful</h1>';
+                $html .= '<p>You have successfully connected to Trans.eu.</p>';
+                $html .= '<p>You can now close this window and refresh the Magento configuration page.</p>';
+                $html .= '<script>setTimeout(function(){ window.close(); }, 3000);</script>';
             } catch (\Exception $e) {
-                $this->messageManager->addErrorMessage(__('Error during token exchange: %1', $e->getMessage()));
+                $html .= '<h1 style="color:red;">Error</h1>';
+                $html .= '<p>' . htmlspecialchars($e->getMessage()) . '</p>';
             }
         } else {
-            $this->messageManager->addErrorMessage(__('Invalid callback request.'));
+            $html .= '<h1 style="color:red;">Invalid Request</h1>';
         }
 
-        $resultRedirect = $this->resultFactory->create(ResultFactory::TYPE_REDIRECT);
-        $resultRedirect->setPath('/');
-        return $resultRedirect;
+        $html .= '</body></html>';
+        $result->setContents($html);
+
+        return $result;
     }
 }
